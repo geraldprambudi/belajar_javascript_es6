3c37ea8b10314b1187476abf6c565c8a90b489f7
geraldprambudi <geraldprambudi94@gmail.com>
2019-08-06 13:39:17 +0700

    #3 variabel const dan let

---
+++ #3_variabel_const_let/main.js
@@ -0,0 +1,16 @@
+// const, let => new es6
+
+//  let
+let avocado = 40;
+avocado = 20;
+console.log(avocado);
+
+// const
+const apple = 40;
+console.log(apple);
+
+// const error
+const apple = 40;
+apple = 20;
+console.log(apple);
+
